2a995f5dd69a8ab266035cdfbbe6b8ce191a7728
jaehue <bbugguj@gmail.com>
2021-03-30 17:20:36 +0800

    Add role to AuthInfo

--- jwtutil/auth_info.go
+++ jwtutil/auth_info.go
@@ -1,19 +1,28 @@
 package jwtutil
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 )
 
 type AuthInfo struct {
-	UserId  int64
-	IsAdmin bool
+	SessionId string
+	UserId    int64
+	Role      string
 }
 
 func GetAuthInfo(c echo.Context) (authInfo AuthInfo) {
+	v := c.Get("user")
+	if v == nil {
+		return GetTokenInfo(c.Request().Header.Get(echo.HeaderAuthorization))
+	}
+
 	user := c.Get("user").(*jwt.Token)
 	if user == nil {
 		return
@@ -38,11 +47,36 @@ func GetAuthInfo(c echo.Context) (authInfo AuthInfo) {
 		return
 	}
 
-	if isAdmin, ok := claims["isAdmin"]; ok {
-		if b, ok := isAdmin.(bool); ok {
-			authInfo.IsAdmin = b
+	if role, ok := claims["role"]; ok {
+		if s, ok := role.(string); ok {
+			authInfo.Role = s
 		}
 	}
 
 	return authInfo
+}
+
+func GetTokenInfo(token string) (info AuthInfo) {
+	ss := strings.Split(token, ".")
+	if len(ss) != 3 {
+		return
+	}
+
+	payload, err := decodeSegment(ss[1])
+	if err != nil {
+		return
+	}
+
+	_ = json.Unmarshal(payload, &info)
+
+	info.SessionId = ss[2]
+	return
+}
+
+func decodeSegment(seg string) ([]byte, error) {
+	if l := len(seg) % 4; l > 0 {
+		seg += strings.Repeat("=", 4-l)
+	}
+
+	return base64.URLEncoding.DecodeString(seg)
 }
